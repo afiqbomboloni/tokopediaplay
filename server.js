771fc27d057f01de1143eb26a73b75d50ff310f2
afiqur31 <afiqurrahman32@gmail.com>
2023-08-14 07:10:31 +0700

    update server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
-const session = require('express-session');
+// const session = require('express-session');
 const db = require("./app/v1/business/models");
 const connectDb = require('./app/v1/data/connections/db.connection');
 const configs = require('./app/config/config' )
@@ -26,11 +26,11 @@ app.use(express.urlencoded({extended: true}));
 connectDb();
 
 // for handling session
-app.use(session({
-    secret: 'secret-key',
-    resave: false,
-    saveUninitialized: true
-  }));
+// app.use(session({
+//     secret: 'secret-key',
+//     resave: false,
+//     saveUninitialized: true
+//   }));
 
 app.get('/', (req, res) => {
     res.json({message: "welcome to tokopedia play api"});
